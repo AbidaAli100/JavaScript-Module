36a4151b2b2e676cd09298ae222a7db94ceb22c0
AbidaAli100 <abida.ali100@gmail.com>
2021-05-23 13:46:44 +0500

    added if else in js

--- class-1/app.js
+++ class-1/app.js
@@ -1,5 +1,12 @@
-  function printMe(){
-    let name = ('name');
-    console.log(name);
-  }
+  function printMe(name){
+    let name1 = name;
+    console.log(name1);
  
+ 
+  var name = alert("Name matched");
+
+  if(name=="ABC") {
+ 
+      alert("Name matched");
+  }
+}
