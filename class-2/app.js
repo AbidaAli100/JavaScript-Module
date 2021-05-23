4e4e0e096c86187edceaa95fad2fe2d6a3285db6
AbidaAli100 <abida.ali100@gmail.com>
2021-05-23 09:46:33 +0500

    added varables in js

---
+++ class-2/app.js
@@ -0,0 +1,34 @@
+function ShowPrice() {
+    let CostofApple = 5;
+    console.log('cost of one apple is:')
+    console.log('CostofApple');
+    console.log('cost of two apple is:')
+    console.log(CostofApple * 2);
+    console.log('cost of three apples is:')
+    console.log(CostofApple * 3);
+    console.log('cost of four apples is:')
+    console.log(CostofApple * 4);
+
+}
+function SaveMe() {
+    let priceofcar = 10;
+    console.log('cost of one car is:')
+    console.log('priceofcar');
+    console.log('cost of two cars is:')
+    console.log(priceofcar * 2);
+    console.log('cost of three cars is:')
+    console.log(priceofcar * 3);
+}
+function ShowAlert() {
+    alert('Hello abida ');
+}
+function ShowMessage() {
+    alert('That is it :D');
+}
+function button() {
+    alert('one');
+    alert('two');
+}
+function Check(){
+    
+}
