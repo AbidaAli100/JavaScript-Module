947bedf1aae2708bd00485e6efc1578b13708f73
AbidaAli100 <abida.ali100@gmail.com>
2021-05-24 20:46:44 +0500

    added JS data and boolean

---
+++ class-4/app.js
@@ -0,0 +1,26 @@
+let cash = 1500;
+console.log (cash);
+
+cash = 1200;
+console.log (cash * 2);
+
+const months = 12;
+console.log(months);
+
+
+function ShowTable(){
+    const table = document.querySelector('input').value;
+    console.log(table);
+    console.log (table * 2);
+    console.log (table * 3);
+    console.log (table * 4);
+    console.log (table * 5);
+    console.log (table * 6);
+}
+let isGood = true;
+
+let name = "Abida";
+alert(name);
+alert('NASREEN')
+
+let email= null;
