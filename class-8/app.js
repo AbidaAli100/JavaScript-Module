f10a4b2e4851c9c4cdea529c7e5cd26a177ee9b4
AbidaAli100 <abida.ali100@gmail.com>
2021-05-27 02:14:57 +0500

    added js loops

---
+++ class-8/app.js
@@ -0,0 +1,30 @@
+//for loop
+
+ for (let i = 0; i < 50; i++) {
+    console.log("ABIDA");
+ }
+ for (let a = 0; a < 2; a++) {
+     console.log("Ali");
+
+     //count till 10
+ }
+for (let abida = 0; abida <= 10; abida++) {
+    console.log(abida);
+
+} //count from 200 to 270
+
+for (let i = 200; i <=220; i+=2){
+    console.log(i);
+} 
+//decriment
+for (let i =  220; i >= 200; i--){
+    console.log (i);
+}
+
+//loops
+
+function CountNumber(){
+  let MinNumb= parseInt( document.querySelector("#minInp").value);
+  let MaxNumb= parseInt( document.querySelector("#maxInp").value);
+  let StepNumb= parseInt( document.querySelector("#stepInp").value);
+}
