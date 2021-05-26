af3f7924827141ded758f4c7f443d23ceaa2b919
AbidaAli100 <abida.ali100@gmail.com>
2021-05-26 22:03:37 +0500

    added if and else if conditions in JS

---
+++ class-7/else-if.js
@@ -0,0 +1,25 @@
+// //else-if 
+// function CheckWeight() {
+//     let Weight = parseInt(document.querySelector("input").value);
+
+//     if (Weight < 50) {
+//         alert("You are Weak");
+//     } else if (Weight > 50 && Weight <= 60) {
+//         alert("You Are Normal");
+//     } else if (Weight > 60) {
+//         alert("You are Fat");
+//     }
+// }
+   //PRACTICE AGAIN//
+function CheckWeight() {
+    let Weight = parseInt(document.querySelector("input").value);
+    if (Weight < 50) {
+        alert("BAJI you are Weak");
+    } else if (Weight > 50 && Weight <= 60) {
+        alert("BAJI you are Normal");
+    } else if (Weight > 60){
+        alert("Baji you are FAT");
+    }
+
+}
+
--- class-7/logical-operators.js
+++ class-7/logical-operators.js
@@ -2,7 +2,7 @@
 /*we need apple, banana and orange for that; 
  but we can make a fruitchart having any two of them */
 
-let IsApple = true;   //is avaibale//
+/*let IsApple = true;   //is avaibale//
 let IsOrange = true;  //is avaiable//
 let IsBanana = false; //is not avaibale//
 
@@ -16,4 +16,30 @@ alert(CanMakeChart);
 
     let CanSurvive = (IsOxygen && IsFood) || (IsOxygen && IsWater) || (IsFood && IsWater);
     alert(CanSurvive);
+
+  //if / else//
+   let ObtMarks = 50;
+
+   if (ObtMarks >= 50) {
+     alert('pass');
+   } else {
+      alert ('fail');
+   }
+  // if condition//
+   let IsNight = true;
+    if (IsNight === true){
+        alert(IsNight)
+    }*/
+
+ const EveningTime = 19; //07:00 PM
+ let hours = new Date().getHours(); //current time (jo b time hoga)
+ if (hours >= EveningTime){
+  alert('TURN THE LIGHT ON');
+}
+
+const Night = 19;
+let CurrentHours = new Date().getHours();
+if (CurrentHours >= Night) {
+    alert('on');
+}
 
